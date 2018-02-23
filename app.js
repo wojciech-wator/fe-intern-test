6f2c062deb0a8faad115a6c541896b109b4e73e4
Filip Mamcarczyk <filip.mamcarczyk@codewise.com>
2018-02-23 13:30:42 +0100

    Described api

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-// Good luck!
+/*
+
+    Use https://codewise-fe-api.herokuapp.com/photos endpoint, to get list of objects that contain photos data, fe:
+
+    {
+        "width":5616,
+        "height":3744,
+        "author":"Alejandro Escamilla",
+        "url":"https://unsplash.com/photos/N7XodRrbzS0/download"
+    }
+
+    This endpoint accepts two query parameters: offset and limit. If you use endpoint like this:
+    https://codewise-fe-api.herokuapp.com/photos?offset=0&limit=5
+    it will return first 5 records from the database,
+
+    if you use it like this:
+    https://codewise-fe-api.herokuapp.com/photos?offset=10&limit=5
+    it will return records no. 10-14 end so on.
+
+    If you don't pass any parameters, offset is set to 0 end limit is set to 50.
+
+    Good luck!
+
+*/
