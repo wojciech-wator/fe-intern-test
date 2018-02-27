b4c3a4d421988a2d2c3ec35fbe6ae4f2b83af26e
Michał Rumanek <michal.rumanek@codewise.com>
2018-02-27 10:56:58 +0100

    note about placeholders parameter

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /*
 
-    Use https://codewise-fe-api.herokuapp.com/photos endpoint, to get list of objects that contain photos data, fe:
+    Use https://codewise-fe-api.herokuapp.com/photos endpoint, to get list of objects that contain photos data, e.g.:
 
     {
         "width":5616,
@@ -9,7 +9,7 @@
         "url":"https://unsplash.com/photos/N7XodRrbzS0/download"
     }
 
-    This endpoint accepts two query parameters: offset and limit. If you use endpoint like this:
+    This endpoint accepts two query parameters: 'offset' and 'limit'. If you use endpoint like this:
     https://codewise-fe-api.herokuapp.com/photos?offset=0&limit=5
     it will return first 5 records from the database,
 
@@ -18,6 +18,10 @@
     it will return records no. 10-14 end so on.
 
     If you don't pass any parameters, offset is set to 0 end limit is set to 50.
+    
+    To use lightweight placeholder images instead of real photos, set truthy value for 'placeholders' parameter, e.g.
+    https://codewise-fe-api.herokuapp.com/photos?placeholders=1, or
+    https://codewise-fe-api.herokuapp.com/photos?offset=10&limit=5&placeholders=1
 
     Good luck!
 
